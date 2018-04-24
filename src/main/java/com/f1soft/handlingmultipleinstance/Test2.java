c53458fb5296a63e7a99c6484bf90e6aaec5e58b
Baburam Neupane <baburam.neupane@f1soft.com>
2018-04-24 12:40:31 +0545

    Test2.java has been added

---
+++ src/main/java/com/f1soft/handlingmultipleinstance/Test2.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.f1soft.handlingmultipleinstance;
+
+/**
+ *
+ * @author babu
+ * Apr 24, 2018 12:36:11 PM
+ */
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("new file added");
+    }
+
+}
